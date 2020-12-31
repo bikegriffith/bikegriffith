78d1e9bad25c64752b07127a030cea893d271e4f
Mike Griffith <bikegriffith@gmail.com>
2020-12-31 00:03:53 -0500

    fix footer; remove unused components

--- src/components/structure/Footer.js
+++ src/components/structure/Footer.js
@@ -1,18 +1,30 @@
 import React from "react";
+import Resume from "../../resume.json";
 
 function Footer() {
   return (
-    <footer className="footer has-background-link">
-      <div className="content has-text-centered has-text-white">
-        <p>
-          <img
-            width="512"
-            height="96"
-            src="https://bulma.io/images/made-with-bulma--semiwhite.png"
-            className="bulma-image"
-            alt=""
-          />
-        </p>
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <div className="columns is-mobile">
+          <div className="column"></div>
+          {Resume.basics.profiles.map((value, index) => {
+            return (
+              <div key={index} className="column has-text-centered">
+                <a
+                  href={value.url}
+                  target="blank"
+                  className="is-hovered"
+                  title={value.network}
+                >
+                  <span className="icon is-medium is-">
+                    <i className={value.x_icon}></i>
+                  </span>
+                </a>
+              </div>
+            );
+          })}
+          <div className="column"></div>
+        </div>
       </div>
     </footer>
   );
